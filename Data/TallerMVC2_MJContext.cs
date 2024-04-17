184edd6b19683d460795191b58aebd55c8c9fcf6
MateoJaramillo26 <105684625+MateoJaramillo26@users.noreply.github.com>
2024-04-17 10:48:30 -0500

    Add project files.

---
+++ Data/TallerMVC2_MJContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using TallerMVC2_MJ.Models;
+
+namespace TallerMVC2_MJ.Data
+{
+    public class TallerMVC2_MJContext : DbContext
+    {
+        public TallerMVC2_MJContext (DbContextOptions<TallerMVC2_MJContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<TallerMVC2_MJ.Models.Burger> Burger { get; set; } = default!;
+    }
+}

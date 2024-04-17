8d8cfac608e75446bd23f9721af904eeb1a2f6ea
MateoJaramillo26 <105684625+MateoJaramillo26@users.noreply.github.com>
2024-04-17 11:36:09 -0500

    Creacion clase Promo y lineas de promo en burguer

---
+++ Models/Promo.cs
@@ -0,0 +1,12 @@
+﻿namespace TallerMVC2_MJ.Models
+{
+    public class Promo
+    {
+        public int PromoId { get; set; }
+        public string? PromoDescripcion { get; set; }
+        public DateTime FechaPromocion { get; set; }
+
+        public int BurguerID { get; set; }
+        public Burger? Burger { get; set; }
+    }
+}
